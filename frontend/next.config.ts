8294bef391e1099eb3ab71a084d8a82b5346ccc1
mdurau <salii.vladimir@gmail.com>
2026-07-05 11:05:51 +0000

    fix: resolve react code architectural problem

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone', // 👈 ADD THIS LINE
+  /* rest of your configuration choices */
 };
 
 export default nextConfig;
